f38c224b39167f5aea66e2d1b53d007ab2c5b9df
Szymon Janowski <szy.janowski@gmail.com>
2018-07-23 11:54:30 +0200

    req update & create pod request

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-import os
-import logging
-import socket
+import os, logging, socket, yaml
 from flask import Flask, jsonify, render_template
+from kubernetes import client, config
+from openshift.dynamic import DynamicClient
 
 HOST_NAME = os.environ.get('OPENSHIFT_APP_DNS', 'localhost')
 APP_NAME = os.environ.get('OPENSHIFT_APP_NAME', 'flask')
@@ -15,6 +15,36 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/add_pod')
+def add_pod():
+    k8s_client = config.new_client_from_config()
+    dyn_client = DynamicClient(k8s_client)
+
+    v1_pod = dyn_client.resources.get(api_version='v1', kind='Pod')
+
+    pod = """
+    kind: Pod
+    metadata:
+      name: alpine
+      namespace: nvidia
+    spec:
+      containers:
+      - image: alpine:latest
+        command:
+          - /bin/sh
+          - "-c"
+          - "sleep 60m"
+        imagePullPolicy: IfNotPresent
+        name: alpine
+      restartPolicy: Always
+    """
+
+    pod_data = yaml.load(pod)
+    resp = v1_pod.create(body=pod_data, namespace='nvidia')
+
+    return resp.metadata
+    
 
 @app.route('/info')
 def info():
